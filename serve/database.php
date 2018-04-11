8af71e38fe9eec2f323515d78f60f0f9a9628234
lewington <lewington@student.unimelb.edu.au>
2018-04-11 23:07:26 +1000

    adding and removing frmo database

--- serve/database.php
+++ serve/database.php
@@ -1,14 +1,27 @@
 <?php
 
+
 try {
   $pdo = new PDO('mysql:host=localhost;dbname=phptest', 'php_basic', 'q1as1z2qwe2');
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   $pdo->exec('SET NAMES "utf8"');
+  if (isset($_GET['delete'])) {
+    $id = $_GET['delete'];
+    $delete = "DELETE FROM people WHERE id = $id";
+    $pdo->exec($delete);
+  } elseif(isset($_GET['name'])) {
+    $insert = 'INSERT INTO people SET name = :name, age = 67';
+    $prep = $pdo->prepare($insert);
+    $prep->bindValue(':name', $_GET['name']);
+    $prep->execute();
+  }
+  $people = $pdo->query('SELECT * FROM people');
 } catch (PDOException $e) {
   echo 'fuck! It fucked up.';
+  echo $e->getMessage();
   exit();
 }
 
-echo 'the connection worked, yay';
+include 'people.html.php'
 
 ?>
---
+++ serve/people.html.php
@@ -0,0 +1,29 @@
+ <!DOCTYPE html>
+ <html lang="en">
+
+<head>
+     <meta charset="UTF-8">
+     <title>TITLE</title>
+     <meta name="description" content="DESCRIPTION">
+    <link rel="stylesheet" href="PATH">
+ </head>
+
+ <body>
+  <h1>People:</h1>
+  <ul>
+    <?php foreach($people as $person): ?>
+      <li>
+        <strong>
+          <?php echo $person['name']
+          ?>
+        </strong>
+        <a href=<?php
+          $id = $person['id'];
+          echo "/database.php?delete=$id";
+        ?>>delete</a>
+      </li>
+    <?php endforeach; ?>
+  </ul>
+ </body>
+
+ </html>
