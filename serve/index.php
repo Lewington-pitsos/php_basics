3fe5a9b54260023e74faa12452a0d4059553750a
lewington <lewington@student.unimelb.edu.au>
2018-04-26 10:21:52 +1000

    logic puzzle site started

---
+++ serve/index.php
@@ -0,0 +1,3 @@
+<?php
+$content = file_get_contents($_SERVER['DOCUMENT_ROOT'] . '/index.html');
+include $_SERVER['DOCUMENT_ROOT'] . '/shared/scaffold.html.php';
---
+++ serve/shared/scaffold.html.php
@@ -0,0 +1,22 @@
+ <!DOCTYPE html>
+ <html lang="en">
+
+<head>
+     <meta charset="UTF-8">
+     <title>TITLE</title>
+     <meta name="description" content="DESCRIPTION">
+    <link rel="stylesheet" href="PATH">
+
+     <!--[if lt IE 9]>
+       <script src = "http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+     <![endif]-->
+ </head>
+
+ <body>
+   <?php echo $content; ?>
+   <footer>
+
+   </footer>
+ </body>
+
+ </html>
